043cceaa5905766961a1012194ee439fcbf67204
Yash Gupta <yash.gupta@anarish.com>
2026-01-12 00:16:54 +0530

    updated code

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,17 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: env('CLIENT_URL', 'http://localhost:5173').split(',').concat(['https://pathinfofrontend.vercel.app', 'https://*.vercel.app']),
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
